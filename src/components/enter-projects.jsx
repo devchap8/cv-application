5ac8171ed4ac410d5fd78d7496ea736fb1563b69
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-05-27 10:29:46 -0400

    Start over

--- src/components/enter-projects.jsx
+++
@@ -1,8 +0,0 @@
-export default function EnterProjects() {
-    return (
-         <section className="enterExperienceSection formSection">
-            <h2 className="sectionHeader">Projects:</h2>
-            
-         </section>
-    );
-}
